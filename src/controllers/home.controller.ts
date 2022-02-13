15a55c59e13a0a391091085d51aed67ec828a2c4
Arda Coşkun <m.ardacoskun@gmail.com>
2022-02-13 21:23:13 +0300

    like and delete ike apis add for stars

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { MovieLikes } from "../entity/MovieLikes";
 import { Movie } from "../entity/Movies";
+import { StarLikes } from "../entity/StarLikes";
+import { Star } from "../entity/Stars";
 import { User } from "../entity/User";
 
 export const localRegister = (req: Request, res: Response) => {
@@ -68,5 +70,60 @@ export const getMovieUpdatePage = async (req: Request, res: Response) => {
   } catch (error) {
     console.log("Like hatası");
     throw new Error(error);
+  }
+};
+
+//Get single movie page
+export const getStarsPage = async (req: Request, res: Response) => {
+  const user = req.user as User;
+
+  try {
+    const currentUser = await User.findOne({ id: user.id });
+
+    const star = await Star.createQueryBuilder("stars")
+      .leftJoinAndSelect("stars.user", "user")
+      .leftJoinAndSelect("stars.comments", "comments.text")
+      .leftJoinAndSelect("stars.likes", "likes")
+      .where("stars.id=:starId", { starId: req.params.id })
+      .getOne();
+
+    if (!star) return res.render("404", { title: "star" });
+
+    const userLikes = await StarLikes.createQueryBuilder("star_likes")
+      .leftJoinAndSelect("star_likes.star", "star")
+      .where("star_likes.owner=:userId", { userId: user.id })
+      .getMany();
+
+    const myLikes = [];
+    for (let i = 0; i < userLikes.length; i++) {
+      myLikes.push(userLikes[i].star.id);
+    }
+
+    res.status(200).render("singleStar", { star, currentUser, myLikes });
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+// Update Page Render
+export const getStarUpdatePage = async (req: Request, res: Response) => {
+  const starId = Number(req.params.id);
+  const user = req.user as User;
+  try {
+    const star = await Star.createQueryBuilder("stars")
+      .leftJoinAndSelect("stars.user", "user")
+      .where("stars.id=:starId", { starId })
+      .getOne();
+
+    if (!star) return res.render("404", { title: "star" });
+
+    if (star.user.id !== user.id) {
+      return res.redirect(`/stars/${starId}`);
+    }
+
+    res.status(200).render("updateStar", { star });
+  } catch (error) {
+    throw new Error(error as string);
   }
 };
